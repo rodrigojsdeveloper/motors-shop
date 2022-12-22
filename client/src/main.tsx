c806ae28b874668b3be5008e7d57baa2638736e8
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-21 22:55:38 -0300

    configured default style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GlobalStyled from "./styles/gobal";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyled />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ client/src/styles/gobal.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+    }
+    body {
+        background: #fff;
+        color: #000;
+    }
+    button {
+        cursor: pointer;
+    }
+    a {
+        text-decoration: none;
+    }
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    
+    input[type=number] {
+        -moz-appearance:textfield;
+    }
+`;
