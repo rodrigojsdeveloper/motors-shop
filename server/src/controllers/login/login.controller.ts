4619aa6f7ef533f4e7b8c9a82db12a0c756e529e
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 15:00:03 -0300

    controller user login created

---
+++ server/src/controllers/login/login.controller.ts
@@ -0,0 +1,14 @@
+import { loginService } from "../../services/login/login.service";
+import { ILogin } from "../../interfaces/login";
+import { Request, Response } from "express";
+
+const loginController = async (req: Request, res: Response) => {
+
+    const data: ILogin = req.body
+
+    const token = await loginService(data)
+
+    return res.json(token)
+}
+
+export { loginController }
