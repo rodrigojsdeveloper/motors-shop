f16f1dabfd930ee8604022f3e6cc296b344264bb
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 11:55:49 -0300

    connection to postgres database created

---
+++ server/src/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm"
+
+require("dotenv").config()
+
+const port = process.env.PG_PORT as number | undefined
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.PG_HOST,
+    port: port,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    logging: true,
+    synchronize: true,
+    entities: [`${__dirname}/**/entities/**/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+})
+
+export { AppDataSource }
