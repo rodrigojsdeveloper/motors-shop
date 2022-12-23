4a8eb9db2393f2c04a5ee59599b656dcd4e457ed
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 15:18:24 -0300

    routes and controller from a user's list of products and token placed in the created user's get and patch routes

--- server/src/routes/users/users.routes.ts
+++ server/src/routes/users/users.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
 
+import { listProductsUserController } from "../../controllers/users/listProductsUser.controller"
 import { createUserController } from "../../controllers/users/createUser.controller"
 import { updateUserController } from "../../controllers/users/updateUser.controller"
+
+import { tokenMiddleware } from "../../middlewares/token.middleware"
 
 
 const routes = Router()
@@ -10,7 +13,9 @@ const usersRoutes = () => {
 
     routes.post("/signup", createUserController)
 
-    routes.patch("/:id", updateUserController)
+    routes.get("/:id", tokenMiddleware, listProductsUserController)
+
+    routes.patch("/:id", tokenMiddleware, updateUserController)
 
     return routes
 }
