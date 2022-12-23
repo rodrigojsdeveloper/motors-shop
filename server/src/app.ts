ccd27e961106d82952329feeb1884f27390e3033
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 16:00:14 -0300

    custom error classes and error middleware created

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
-import express from "express"
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware"
 import { appRoutes } from "./routes"
+import "express-async-error"
+import express from "express"
 
 const app = express()
 app.use(express.json())
 
 appRoutes(app)
+
+app.use(handleErrorMiddleware)
 
 export { app }
---
+++ server/src/middlewares/handleError.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../helpers";
+
+const handleErrorMiddleware = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof ApiError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({ message: "Interval server error" });
+};
+
+export { handleErrorMiddleware };
