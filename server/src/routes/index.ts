86ec46f8d0c9294d1ea572ba26079008529106b7
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 17:39:31 -0300

    created product routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Express } from "express"
 
+import { productsRoutes } from "./products/products.routes"
 import { usersRoutes } from "./users/users.routes"
 import { loginRoutes } from "./login/login.routes"
 
@@ -7,6 +8,7 @@ const appRoutes = (app: Express) => {
 
     app.use("/users", usersRoutes())
     app.use("/signin", loginRoutes())
+    app.use("/products", productsRoutes())
 }
 
 export { appRoutes }
---
+++ server/src/routes/products/products.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import { listAllProductsController } from "../../controllers/products/listAllProducts.controller";
+import { createProductController } from "../../controllers/products/createProduct.controller";
+import { deleteProductController } from "../../controllers/products/deleteProduct.controller";
+import { updateProductController } from "../../controllers/products/updateProduct.controller";
+
+import { tokenMiddleware } from "../../middlewares/token.middleware";
+
+
+const routes = Router()
+
+const productsRoutes = () => {
+
+    routes.post("", tokenMiddleware, createProductController)
+
+    routes.get("", tokenMiddleware, listAllProductsController)
+
+    routes.patch("/:id", tokenMiddleware, updateProductController)
+
+    routes.delete("/:id", tokenMiddleware, deleteProductController)
+
+    return routes
+}
+
+export { productsRoutes }
