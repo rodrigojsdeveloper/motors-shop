bdb81e644710645e46219adfb7d17d7256813264
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-23 15:01:58 -0300

    routes user login created

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
-import { usersRoutes } from "./users/users.routes"
 import { Express } from "express"
+
+import { usersRoutes } from "./users/users.routes"
+import { loginRoutes } from "./login/login.routes"
 
 const appRoutes = (app: Express) => {
 
     app.use("/users", usersRoutes())
+    app.use("/signin", loginRoutes())
 }
 
 export { appRoutes }
