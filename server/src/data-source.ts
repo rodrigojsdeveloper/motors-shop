66e9c2f60d8fd052709055cfa19db4ae3d12cfcd
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-17 12:42:37 -0300

    build.sh file added to deploy in Render

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,19 +1,36 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
+import "reflect-metadata";
+import path from "path";
 
 require("dotenv").config();
 
 const port = process.env.POSTGRES_PORT as number | undefined;
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: port,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: false,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-});
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{ts,js}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{ts,js}");
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
-export { AppDataSource };
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: port,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+export const AppDataSource = new DataSource(dataSourceConfig());
