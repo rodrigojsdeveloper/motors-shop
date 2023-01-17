a8d616ab8ed581d0753fbab0cd50f48535341af6
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-17 11:40:15 -0300

    docker on server

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,15 +2,15 @@ import { DataSource } from "typeorm";
 
 require("dotenv").config();
 
-const port = process.env.PG_PORT as number | undefined;
+const port = process.env.POSTGRES_PORT as number | undefined;
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.PG_HOST,
+  host: process.env.POSTGRES_HOST,
   port: port,
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: false,
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
