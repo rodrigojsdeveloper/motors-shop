c3c3cde45126b487f40b0f9cf6a7fb3819ae9180
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-02-12 20:12:37 -0300

    local service exchange, for production

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://motors-shop-z6i7.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
