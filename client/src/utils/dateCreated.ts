4d9ec62d6afba40d18e28c484ac6e1e4b51b5aee
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-02-12 23:28:35 -0300

    comments and bids showed the hours, minutes and seconds of the time it was posted with the new implementation in the ShowDate function

--- client/src/utils/dateCreated.ts
+++ client/src/utils/dateCreated.ts
@@ -1,26 +1,42 @@
-const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+const ONE_HOUR_IN_MS = 1000 * 60 * 60;
 
 const showDate = (date: string) => {
   const currentDate = new Date();
   const diffInTime = +currentDate - +new Date(date);
-  const diffInDays = diffInTime / ONE_DAY_IN_MS;
-  const postedAt = Math.floor(diffInDays);
+  const diffInHours = diffInTime / ONE_HOUR_IN_MS;
+  const postedAt = Math.floor(diffInHours);
 
-  if (diffInDays >= 365) {
-    const years = Math.floor(diffInDays / 365);
+  if (diffInHours >= 24) {
+    const diffInDays = diffInHours / 24;
 
-    return `há ${years} ${years > 1 ? "anos" : "ano"}`;
-  } else if (diffInDays >= 30) {
-    const months = Math.floor(diffInDays / 30);
+    if (diffInDays >= 365) {
+      const years = Math.floor(diffInDays / 365);
 
-    return `há ${months} ${months > 1 ? "meses" : "mês"}`;
+      return `há ${years} ${years > 1 ? "anos" : "ano"}`;
+    } else if (diffInDays >= 30) {
+      const months = Math.floor(diffInDays / 30);
+
+      return `há ${months} ${months > 1 ? "meses" : "mês"}`;
+    } else {
+      const days = Math.floor(diffInDays);
+
+      return `há ${days} ${days > 1 ? "dias" : "dia"}`;
+    }
   } else if (postedAt > 1) {
-    return `há ${postedAt} dias`;
-  } else if (postedAt === 0) {
-    return "Postado hoje";
-  }
+    return `há ${postedAt} horas`;
+  } else if (postedAt === 1) {
+    return `há ${postedAt} hora`;
+  } else {
+    const diffInSeconds = diffInTime / 1000;
+    if (diffInSeconds < 60) {
+      const seconds = Math.round(diffInSeconds);
+      return `há ${seconds} ${seconds > 1 ? "segundos" : "segundo"}`;
+    }
+    const diffInMinutes = diffInTime / (1000 * 60);
+    const minutes = Math.floor(diffInMinutes);
 
-  return `há ${postedAt} dia`;
+    return `há ${minutes} ${minutes > 1 ? "minutos" : "minuto"}`;
+  }
 };
 
 export { showDate };
