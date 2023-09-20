c6d60a7f53a7fd409d2d98b53e42e3fcb7ccf645
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-20 01:30:51 -0300

    refactor: refactor deploy code

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,28 +1,35 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import path from "path";
 import "dotenv/config";
 
-const entitiesPath: string = path.join(__dirname, "./entities/*.{ts,js}");
-const migrationsPath: string = path.join(__dirname, "./migrations/*.{ts,js}");
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{ts,js}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{ts,js}");
 
-const AppDataSource =
-  process.env.NODE_ENV === "test"
-    ? new DataSource({
-        type: "sqlite",
-        database: ":memory:",
-        entities: [entitiesPath],
-        synchronize: true,
-      })
-    : new DataSource({
-        type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: false,
-        entities: [entitiesPath],
-        migrations: [migrationsPath],
-      });
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const AppDataSource = new DataSource(dataSourceConfig());
 
 export { AppDataSource };
